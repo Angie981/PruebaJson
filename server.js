96e3c91deee6ea3569478db66af095de107dfb73
Angie Ruth Palacios Peña <Angie.Palacios@tc1labs.com>
2023-04-20 18:20:25 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const datos = require('./datos.json');
+const app = express();
+
+app.get('/datos', (req, res) => {
+  res.json(datos);
+});
+
+app.get('/datos/:fecha_inicio/:fecha_fin', (req, res) => {
+  const fechaInicio = new Date(req.params.fecha_inicio);
+  const fechaFin = new Date(req.params.fecha_fin);
+
+  const datosFiltrados = datos.filter(dato => {
+    const fechaDato = new Date(dato.fecha_creacion);
+    return fechaDato >= fechaInicio && fechaDato <= fechaFin;
+  });
+
+  res.json(datosFiltrados);
+});
+
+app.get('/datos/fechas', (req, res) => {
+    const fechas = datos.map(dato => new Date(dato.fecha_creacion));
+    const fechaMinima = new Date(Math.min(...fechas));
+    const fechaMaxima = new Date(Math.max(...fechas));
+    res.json({ fechaMinima, fechaMaxima });
+  });
+
+app.listen(3000, () => {
+  console.log('Servidor iniciado en el puerto 3000');
+});
